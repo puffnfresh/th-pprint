77cfcf0f02d34b2bb65b5aea530ca40a9fe18075
Brian McKenna <brian@brianmckenna.org>
2017-11-06 16:50:49 +1100

    Add a Haddock comment and export the main function

--- Language/Haskell/TH/Cleanup.hs
+++ Language/Haskell/TH/Cleanup.hs
@@ -1,5 +1,6 @@
 module Language.Haskell.TH.Cleanup (
   renderNoLength
+, simplifiedTH
 ) where
 
 import Control.Lens
@@ -13,6 +14,11 @@ renderNoLength :: Ppr a => a -> String
 renderNoLength =
   HPJ.renderStyle (HPJ.style { HPJ.lineLength = maxBound }) . to_HPJ_Doc . ppr
 
+-- | Simplifies and pretty-prints declarations. Will give back a quoted string.
+--
+-- Can be used from GHCi like so:
+--
+-- > putStrLn $(simplifiedTH =<< makePrisms ''Either)
 simplifiedTH :: [Dec] -> ExpQ
 simplifiedTH =
   stringE . renderNoLength . fmap (transformOf decChildren simplifyDec . removeAllModNames)
